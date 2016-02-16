02b5608835c7100bc1232d39f3f68503eced7994
Chris Breiding <chrisbreiding@gmail.com>
2016-02-15 20:53:49 -0500

    extract timer editor

---
+++ src/components/timer-editor.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import Modal from './modal';
+import propTypes from '../lib/prop-types';
+import { toMinutesAndSeconds, fromMinutesAndSeconds, padNumber } from '../lib/time';
+
+function adjustedTime(time) {
+  if (time < 0) {
+    return 60 + time;
+  } else {
+    return time >= 60 ? time - 60 : time;
+  }
+}
+
+function TimerEditor (props) {
+  const { minutes, seconds } = toMinutesAndSeconds(props.time);
+
+  const changeMinutes = amount => () =>
+    props.onUpdateTime(fromMinutesAndSeconds(adjustedTime(minutes + amount), seconds));
+  const changeSeconds = amount => () =>
+    props.onUpdateTime(fromMinutesAndSeconds(minutes, adjustedTime(seconds + amount)));
+
+  return (
+    <Modal onClose={() => props.onEdit(false)}>
+      <div className="timer-editor">
+        <div className="wrap">
+          <button onClick={changeMinutes(5)}><i className="fa fa-chevron-up"></i> 5</button>
+          <button onClick={changeMinutes(1)}><i className="fa fa-chevron-up"></i> 1</button>
+          <div className="content">{padNumber(minutes)}</div>
+          <button onClick={changeMinutes(-1)}><i className="fa fa-chevron-down"></i> 1</button>
+          <button onClick={changeMinutes(-5)}><i className="fa fa-chevron-down"></i> 5</button>
+        </div>
+        <div className="wrap separator">
+          <button></button>
+          <button></button>
+          <div className="content">:</div>
+          <button></button>
+          <button></button>
+        </div>
+        <div className="wrap">
+          <button onClick={changeSeconds(5)}><i className="fa fa-chevron-up"></i> 5</button>
+          <button onClick={changeSeconds(1)}><i className="fa fa-chevron-up"></i> 1</button>
+          <div className="content">{padNumber(seconds)}</div>
+          <button onClick={changeSeconds(-1)}><i className="fa fa-chevron-down"></i> 1</button>
+          <button onClick={changeSeconds(-5)}><i className="fa fa-chevron-down"></i> 5</button>
+        </div>
+      </div>
+    </Modal>
+  );
+}
+
+TimerEditor.propTypes = propTypes.timer;
+
+export default TimerEditor;
--- src/components/timer.jsx
+++ src/components/timer.jsx
@@ -1,55 +1,8 @@
 import cs from 'classnames';
 import React from 'react';
-import Modal from './modal';
+import Editor from './timer-editor';
 import propTypes from '../lib/prop-types';
 import { toMinutesAndSeconds, fromMinutesAndSeconds, padNumber } from '../lib/time';
-
-function adjustedTime(time) {
-  if (time < 0) {
-    return 60 + time;
-  } else {
-    return time >= 60 ? time - 60 : time;
-  }
-}
-
-function editor (props) {
-  if (!props.isEditing) { return null; }
-
-  const { minutes, seconds } = toMinutesAndSeconds(props.time);
-
-  const changeMinutes = amount => () =>
-    props.onUpdateTime(fromMinutesAndSeconds(adjustedTime(minutes + amount), seconds));
-  const changeSeconds = amount => () =>
-    props.onUpdateTime(fromMinutesAndSeconds(minutes, adjustedTime(seconds + amount)));
-
-  return (
-    <Modal onClose={() => props.onEdit(false)}>
-      <div className="timer-editor">
-        <div className="wrap">
-          <button onClick={changeMinutes(5)}><i className="fa fa-chevron-up"></i> 5</button>
-          <button onClick={changeMinutes(1)}><i className="fa fa-chevron-up"></i> 1</button>
-          <div className="content">{padNumber(minutes)}</div>
-          <button onClick={changeMinutes(-1)}><i className="fa fa-chevron-down"></i> 1</button>
-          <button onClick={changeMinutes(-5)}><i className="fa fa-chevron-down"></i> 5</button>
-        </div>
-        <div className="wrap separator">
-          <button></button>
-          <button></button>
-          <div className="content">:</div>
-          <button></button>
-          <button></button>
-        </div>
-        <div className="wrap">
-          <button onClick={changeSeconds(5)}><i className="fa fa-chevron-up"></i> 5</button>
-          <button onClick={changeSeconds(1)}><i className="fa fa-chevron-up"></i> 1</button>
-          <div className="content">{padNumber(seconds)}</div>
-          <button onClick={changeSeconds(-1)}><i className="fa fa-chevron-down"></i> 1</button>
-          <button onClick={changeSeconds(-5)}><i className="fa fa-chevron-down"></i> 5</button>
-        </div>
-      </div>
-    </Modal>
-  );
-}
 
 function Timer (props) {
   const { minutes, seconds } = toMinutesAndSeconds(props.timeLeft);
@@ -67,7 +20,7 @@ function Timer (props) {
       <button className="start" onClick={props.onStart}>Start</button>
       <button className="stop" onClick={props.onStop}>Stop</button>
       <button className="remove" onClick={props.onRemove}>x</button>
-      {editor(props)}
+      {props.isEditing ? <Editor {...props} /> : null}
     </li>
   );
 }
