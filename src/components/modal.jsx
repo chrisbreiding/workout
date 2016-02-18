3f07a2238356ecd795f191abaaa914f300eea6f1
Chris Breiding <chrisbreiding@gmail.com>
2016-02-17 21:39:27 -0500

    simplify modal

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -2,64 +2,24 @@ import React, { createClass, PropTypes as PT } from 'react';
 
 export default createClass({
   propTypes: {
-    className: PT.string,
-    headerContent: PT.node,
-    footerContent: PT.node,
-    onCancel: PT.func,
-    onOk: PT.func,
-    onClose: PT.func
+    className: PT.string
   },
 
   componentDidMount () {
-    document.body.className += 'modal-dialog-present';
+    document.body.className += ' modal-dialog-present';
   },
 
   componentWillUnmount () {
-    document.body.className = '';
+    document.body.className = document.body.className.replace(' modal-dialog-present', '');
   },
 
   render () {
     return (
-      <div className={`modal-dialog-overlay ${this.props.className}`}>
+      <div className={`modal-dialog-overlay ${this.props.className || ''}`}>
         <div className="modal-dialog">
-          <header>
-            {this.props.headerContent}
-            {this._closeButton()}
-          </header>
-          <div className="modal-dialog-content">
-            {this.props.children}
-          </div>
-          <footer>
-            {this.props.footerContent}
-            <div className="modal-controls">
-              {this._cancelButton()}
-              {this._okButton()}
-            </div>
-          </footer>
+          {this.props.children}
         </div>
       </div>
     );
-  },
-
-  _cancelButton () {
-    if (!this.props.onCancel) { return null; }
-
-    return <button className="cancel" onClick={this.props.onCancel}>Cancel</button>;
-  },
-
-  _okButton () {
-    if (!this.props.onOk) { return null; }
-
-    return <button className="ok" onClick={this.props.onOk}>OK</button>;
-  },
-
-  _closeButton () {
-    if (!this.props.onClose) { return null; }
-
-    return (
-      <button className="close" title="Close" onClick={this.props.onClose}>
-        <i className="fa fa-times"></i>
-      </button>
-    );
-  },
+  }
 });
