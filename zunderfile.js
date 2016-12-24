2ab3ac040abc7342186d62c62e66c3f75d3984d9
Chris Breiding <chrisbreiding@gmail.com>
2016-12-24 17:12:15 -0500

    create app cache manifest

--- zunderfile.js
+++ zunderfile.js
@@ -1,4 +1,16 @@
 require('zunder').setConfig({
+  appCache: true,
+  appCacheTransform (files) {
+    return files.map((file) => {
+      if (/fontawesome/.test(file)) {
+        return `${file}?v=4.7.0`
+      }
+      if (/CNAME/.test(file)) {
+        return null
+      }
+      return file
+    })
+  },
   staticGlobs: {
     'static/**': '',
     'node_modules/font-awesome/fonts/**': '/fonts',
